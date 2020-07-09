c32b4d5d32840acb96c55ad1d7ab7a39588c34d0
Nick Gelotte <ngelotte@r2cgroup.com>
2020-07-08 22:52:19 -0700

    Initial Build

---
+++ SudoExample.cpp
@@ -0,0 +1,39 @@
+﻿// SudoExample.cpp : Defines the entry point for the application.
+//
+
+#include "SudoExample.h"
+#include <iostream>		// Include all needed libraries here
+extern "C" {
+#include <wiringPi.h>
+}
+
+using namespace std;		// No need to keep using “std”
+
+int main()
+{
+	int l;
+	if (wiringPiSetup() == -1)
+	{
+		exit(1);
+	}
+
+	pinMode(1, PWM_OUTPUT);	// Configure GPIO1 as a PWM_OUTPUT
+
+	// Main program loop
+	while (1)
+	{
+
+		for (l = 0; l < 1024; ++l)
+		{
+			pwmWrite(1, l);
+			delay(1);
+		}
+
+		for (l = 1023; l >= 0; --l)
+		{
+			pwmWrite(1, l);
+			delay(1);
+		}
+
+	}
+}
